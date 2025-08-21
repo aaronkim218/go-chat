9d2eb989609fb7b39eff5e09cd94ab6a76b176b2
Aaron Kim <44210329+aaronkim218@users.noreply.github.com>
2025-08-21 16:27:36 -0400

    update loading screen (#171)

--- frontend/src/components/layouts/BaseLayout.tsx
+++ frontend/src/components/layouts/BaseLayout.tsx
@@ -1,12 +1,17 @@
 import { Outlet } from "react-router-dom";
 import { useUserContext } from "@/contexts/User";
 import { Toaster } from "sonner";
+import { LoaderCircle } from "lucide-react";
 
 const BaseLayout = () => {
   const { firstLoad } = useUserContext();
 
   return firstLoad ? (
-    <div>Loading...</div>
+    <div className=" flex flex-col justify-center items-center h-full gap-3">
+      <LoaderCircle size={"8rem"} className="animate-spin" />
+      <p className=" text-3xl">Loading...</p>
+      <p>(this could take a few minutes due to cold server start)</p>
+    </div>
   ) : (
     <>
       <Outlet />
